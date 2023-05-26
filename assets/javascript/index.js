edde83eb48c9bfcd3c013d2489d7b0fd8971b149
Cesar A. Roman <130246064+Cromananimation@users.noreply.github.com>
2023-05-25 21:49:30 -0500

    fixing timer issues

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,10 +4,28 @@ for(var i =num; i >0 ; i-- ) {
 
 console.log(i);
 }
+function timeInterval(seconds) 
 
-
-
+setInterval(function () {
+// As long as the `timeLeft` is greater than 1
+if (timeLeft > 1) {
+  // Set the `textContent` of `timerEl` to show the remaining seconds
+  timerElement.textContent = timeLeft + ' seconds remaining';
+  // Decrement `timeLeft` by 1
+  timeLeft--;
+} else if (timeLeft === 1) {
+  // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+  timerElement.textContent = timeLeft + ' second remaining';
+  timeLeft--;
+} else {
+  // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+  timerElement.textContent = '';
+  // Use `clearInterval()` to stop the timer
+  clearInterval(timeInterval);
+  // Call the `displayMessage()` function
+  displayMessage("Times Up");
+}
+}, 1000),
 
 };
 
-function(75);
--- assets/javascript/quiz-quest.js
+++ assets/javascript/quiz-quest.js
@@ -1,25 +1,5 @@
 var questions = [
-    function timeInterval() 
-        setInterval(function (75) {
-        // As long as the `timeLeft` is greater than 1
-        if (timeLeft > 1) {
-          // Set the `textContent` of `timerEl` to show the remaining seconds
-          timerElement.textContent = timeLeft + ' seconds remaining';
-          // Decrement `timeLeft` by 1
-          timeLeft--;
-        } else if (timeLeft === 1) {
-          // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-          timerElement.textContent = timeLeft + ' second remaining';
-          timeLeft--;
-        } else {
-          // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-          timerElement.textContent = '';
-          // Use `clearInterval()` to stop the timer
-          clearInterval(timeInterval);
-          // Call the `displayMessage()` function
-          displayMessage("Times Up");
-        }
-      }, 1000),
+   
 {
     question: "Which is not a JS data type.",
     choices: ['cookies', 'booleans', 'Arrays', 'Variables'],
@@ -43,7 +23,7 @@ var questions = [
 },
 {
     question: "How many Errors types in JavaScript?",
-    choices: ['5', '3', '8', '6']:
+    choices: ['5', '3', '8', '6'],
     answer: '3- Syntax, Runtime, and Logical Errors'
 }
 
